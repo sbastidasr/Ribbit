9dfd7f8483e753f6258e7c9f27e8dcb78ae80a67
Sebastian Bastidas <sbastidasr@Sebastians-MacBook-Pro.local>
2014-06-07 12:35:45 -0500

    Initial commit local git

---
+++ Ribbit/AQAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AQAppDelegate.h
+//  Ribbit
+//
+//  Created by Sebastian Bastidas on 6/7/14.
+//  Copyright (c) 2014 Adtq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AQAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
